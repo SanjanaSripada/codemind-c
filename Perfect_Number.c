d60025301f3dd83aa0436d01fe840a9db9f8f69d
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2022-12-12 10:40:29 +0530

    #include<stdio.h> int main() {   int n,r,s=0,i;   scanf("%d",&n);   for(i=1;i<=(n-1);i++)   {       r=n%i;       if(r==0)       {           s=s+i;       }   }   if(s==n)   printf("True");   else   printf("False");   return 0; }

---
+++ Perfect_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int n,r,s=0,i;
+  scanf("%d",&n);
+  for(i=1;i<=(n-1);i++)
+  {
+      r=n%i;
+      if(r==0)
+      {
+          s=s+i;
+      }
+  }
+  if(s==n)
+  printf("True");
+  else
+  printf("False");
+  return 0;
+}
