6d9048c721dcf0ce9bf7c913e468e46ad1f06dd0
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2022-12-26 09:59:53 +0530

    #include<stdio.h> int main() {     int W,X,Y,Z;     scanf("%d%d%d%d",&W,&X,&Y,&Z);     if(W==X)     {         printf("YES");     }     else if(W==Y)     {         printf("YES");     }     else if(W==Z)     {         printf("YES");     }     else if(W==(X+Y))     {         printf("YES");     }     else if(W==(Y+Z))     {         printf("YES");     }     else if(W==(Z+X))     {         printf("YES");     }     else if(W==(X+Y+Z))     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Measuring_the_Object.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int W,X,Y,Z;
+    scanf("%d%d%d%d",&W,&X,&Y,&Z);
+    if(W==X)
+    {
+        printf("YES");
+    }
+    else if(W==Y)
+    {
+        printf("YES");
+    }
+    else if(W==Z)
+    {
+        printf("YES");
+    }
+    else if(W==(X+Y))
+    {
+        printf("YES");
+    }
+    else if(W==(Y+Z))
+    {
+        printf("YES");
+    }
+    else if(W==(Z+X))
+    {
+        printf("YES");
+    }
+    else if(W==(X+Y+Z))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
