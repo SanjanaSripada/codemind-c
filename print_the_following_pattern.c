7e0c8f656eedbfec33e33f980b396253e017f70d
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2023-01-02 10:49:55 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,8 +5,10 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=n;j>=1;j--)
-        printf("%d ",j);
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j); 
+        }
         printf("
 ");
     }
