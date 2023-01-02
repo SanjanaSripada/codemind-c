4032d85405a42ebdd38ad3f355cb93902c78703f
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2023-01-02 11:38:10 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=12;i++)     {         int p;         p=n*i;         printf("%d x %d = %d ",n,i,p);     } }

---
+++ Multiplication_Table_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=12;i++)
+    {
+        int p;
+        p=n*i;
+        printf("%d x %d = %d
+",n,i,p);
+    }
+}
