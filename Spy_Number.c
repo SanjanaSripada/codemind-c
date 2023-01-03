caff0acde0990234f1eae457a5538513ce7e2d72
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2023-01-03 10:01:55 +0530

    #include<stdio.h> int main() {     int n,s=0,p=1,rem;     scanf("%d",&n);     while(n!=0)     {         rem=n%10;         s=s+rem;         p=p*rem;         n=n/10;     }     if(s==p)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,s=0,p=1,d;
+    int n,s=0,p=1,rem;
     scanf("%d",&n);
-    for(;n>0;)
+    while(n!=0)
     {
-        d=n%10;
-        s=s+d;
-        p=p*d;
+        rem=n%10;
+        s=s+rem;
+        p=p*rem;
         n=n/10;
     }
     if(s==p)
-    printf("Spy Number");
+    {
+        printf("Spy Number");
+    }
     else
-    printf("Not Spy Number");
+    {
+        printf("Not Spy Number");
+    }
 }
