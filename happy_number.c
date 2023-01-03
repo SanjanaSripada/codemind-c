a461f8aa100bb94f8aced934093722e740fef9cd
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2023-01-03 11:50:09 +0530

    #include<stdio.h> int main() {     int n,rem,sum=0;     scanf("%d",&n);     while(sum!=1 && sum!=4)     {         sum=0;         while(n>0)         {             rem=n%10;             sum=sum+(rem*rem);             n=n/10;         }         n=sum;     }     if(sum==1 or sum==7)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ happy_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,sum=0;
+    scanf("%d",&n);
+    while(sum!=1 && sum!=4)
+    {
+        sum=0;
+        while(n>0)
+        {
+            rem=n%10;
+            sum=sum+(rem*rem);
+            n=n/10;
+        }
+        n=sum;
+    }
+    if(sum==1 or sum==7)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
