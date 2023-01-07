8e1bf11833f983b8bc6ec47d86ff5b2a162fc96a
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2023-01-07 18:52:04 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>b && a>c)     {         if(b>c)         printf("%d",a+b);         else         printf("%d",a+c);     }     else if(b>c && b>a)     {         if(c>a)         printf("%d",b+c);         else         printf("%d",b+a);     }     else     {         if(a>b)         printf("%d",a+c);         else         printf("%d",b+c);     } }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b && a>c)
+    {
+        if(b>c)
+        printf("%d",a+b);
+        else
+        printf("%d",a+c);
+    }
+    else if(b>c && b>a)
+    {
+        if(c>a)
+        printf("%d",b+c);
+        else
+        printf("%d",b+a);
+    }
+    else
+    {
+        if(a>b)
+        printf("%d",a+c);
+        else
+        printf("%d",b+c);
+    }
+}
