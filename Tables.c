dd0ffd0aedfcd22440d5cab001bec5267b62ed51
SanjanaSripada <119917997+SanjanaSripada@users.noreply.github.com>
2023-05-22 14:42:30 +0530

    #include<stdio.h> int main() {     int n,p=1,i,a;     scanf("%d%d",&a,&n);     for(i=1;i<=n;i++)     {         if(i%2!=0)         {             p=a*i;             printf("%d x %d = %d ",a,i,p);         }     } }

--- Tables.c
+++ Tables.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
-int main() {
-  int n,j,i;
-  scanf("%d%d", &n,&j);
-  for (i = 1; i <=j; ++i)
-  {
-    if(i%2!=0)
-    printf("%d x %d = %d
-",n,i,n*i);
-  }
-  return 0;
+int main()
+{
+    int n,p=1,i,a;
+    scanf("%d%d",&a,&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            p=a*i;
+            printf("%d x %d = %d
+",a,i,p);
+        }
+    }
 }
